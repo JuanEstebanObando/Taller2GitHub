e635df063098c9ea414c3feb0693282592f880bd
JuanEstebanObando <juanesteban.obando@udea.edu.co>
2024-04-19 19:48:44 -0500

    Adding video-views structures

---
+++ src/main/java/cultureMedia/model/View.java
@@ -0,0 +1,9 @@
+package cultureMedia.model;
+
+import java.time.LocalDateTime;
+
+
+	
+public record View(String userFullName, LocalDateTime startPlayingTime, Integer age, Video video) {
+
+}
---
+++ src/main/java/cultureMedia/repository/ViewsRepository.java
@@ -0,0 +1,7 @@
+package cultureMedia.repository;
+
+import cultureMedia.model.View;
+
+public interface ViewsRepository {
+	View save(View save);
+}
