5361e96d63ee4a57043f02d85d0ce6e347f3cd7a
JuanEstebanObando <juanesteban.obando@udea.edu.co>
2024-04-24 18:39:39 -0500

    Adding video-views structures

---
+++ src/main/java/cultureMedia/exception/VideoNotFoundException.java
@@ -0,0 +1,12 @@
+package cultureMedia.exception;
+
+public class VideoNotFoundException extends Exception {
+	public VideoNotFoundException(String titulo) {
+		super();
+		
+	}
+	public VideoNotFoundException() {
+		super();
+		
+	}
+}
