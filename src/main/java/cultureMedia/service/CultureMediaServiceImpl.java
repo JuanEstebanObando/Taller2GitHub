e44cfe9bf948f1cbcf61b8ecb0a5e6830cd82217
JuanEstebanObando <juanesteban.obando@udea.edu.co>
2024-04-24 19:23:46 -0500

    Add service and implement service

---
+++ src/main/java/cultureMedia/service/CultureMediaServiceImpl.java
@@ -0,0 +1,25 @@
+package cultureMedia.service;
+
+import java.util.List;
+
+import cultureMedia.model.Video;
+import cultureMedia.model.View;
+
+public class CultureMediaServiceImpl implements CultureMediaService {
+
+	@Override
+	public List<Video> findAll() {
+		return null;
+	}
+
+	@Override
+	public Video save(Video save) {
+		return null;
+	}
+
+	@Override
+	public View save(View save) {
+		return null;
+	}
+	
+}
