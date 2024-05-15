5d71e4f0b2886b1cfeda67d6bcc057d769531e5a
JuanEstebanObando <juanesteban.obando@udea.edu.co>
2024-05-15 17:31:59 -0500

    Adding api components

--- src/main/java/cultureMedia/AplicationRunner.java
+++ src/main/java/cultureMedia/AplicationRunner.java
@@ -1,5 +1,18 @@
 package cultureMedia;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAutoConfiguration
+
 public class AplicationRunner {
+
+	public static void main(String[] args) {
+			SpringApplication.run(AplicationRunner.class, args);
+	}
 
 }
--- src/main/java/cultureMedia/controllers/CultureMediaController.java
+++ src/main/java/cultureMedia/controllers/CultureMediaController.java
@@ -3,8 +3,13 @@ package cultureMedia.controllers;
 import java.util.*;
 import cultureMedia.exception.VideoNotFoundException;
 import cultureMedia.model.Video;
+import cultureMedia.repository.impl.VideoRepositoryImpl;
+import cultureMedia.repository.impl.ViewsRepositoryImpl;
 import cultureMedia.service.CultureMediaService;
+import cultureMedia.service.CultureMediaServiceImpl;
 
+import org.springframework.web.bind.annotation.*;
+@RestController
 public class CultureMediaController {
 
 	private final CultureMediaService cultureMediaService;
@@ -13,11 +18,16 @@ public class CultureMediaController {
 	public CultureMediaController(CultureMediaService cultureMediaService) {
 		this.cultureMediaService = cultureMediaService;
 	}
+	
+	public CultureMediaController() {this.cultureMediaService = new CultureMediaServiceImpl(new VideoRepositoryImpl(),new ViewsRepositoryImpl());}
 
-
-	public List<Video> find_allVideos() throws VideoNotFoundException {
+	@GetMapping("/videos")
+	public List<Video> findAll() throws VideoNotFoundException {
 		return cultureMediaService.findAll();
 	}
 
-
+	@PostMapping("/videos")
+    public Video agregar(@RequestBody Video video) {
+        return cultureMediaService.save(video);
+    }
 }
